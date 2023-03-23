88e19dd80dd3266a21412656228a1be21668b1f2
letai2001 <nguyenletai16122001@gmail.com>
2023-03-23 10:57:58 +0700

    gop

--- gop.py
+++ gop.py
@@ -1,11 +1,11 @@
 import pandas as pd
 # Load data from CSV files
-df1 = pd.read_csv('merged_giaydep.csv')
-df2 = pd.read_csv('merged_thoitrangnam.csv')
-df3 = pd.read_csv('merged.csv')
-# df4 = pd.read_csv('all_item_dochoi_1200_1820.csv')
+df1 = pd.read_csv('merged_all_local.csv')
+df2 = pd.read_csv('merged_all.csv')
+# df3 = pd.read_csv('all_item_diengiadung_301_600.csv')
+# df4 = pd.read_csv('all_item_diengiadung_600_850.csv')
 # Concatenate dataframes vertically
-df_concat = pd.concat([df1, df2  , df3])
+df_concat = pd.concat([df1, df2])
 df_concat = df_concat.reset_index(drop=True)
 df_concat.index += 1
 
@@ -13,4 +13,4 @@ df_concat.index += 1
 # Save concatenated dataframe to a new CSV file
 # df_concat = df_concat.drop('Unnamed: 0', axis=1)
 
-df_concat.to_csv('merged_all.csv', index_label='id')
+df_concat.to_csv('merged_all_in.csv', index_label='id')
