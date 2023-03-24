921fd8b145d3ccd6441d62ff0e7afb85e65f32cf
letai2001 <nguyenletai16122001@gmail.com>
2023-03-24 10:32:06 +0700

    f

--- crawl.py
+++ crawl.py
@@ -7,6 +7,7 @@ from selenium.common.exceptions import NoSuchElementException, ElementNotInterac
 from selenium.webdriver.common.by import By
 import pandas as pd
 import itertools
+from crawl_category import TikiScraper
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
@@ -15,21 +16,26 @@ chrome_options.add_argument(f'user-agent={user_agent}')
 
 driver = webdriver.Chrome("C:\\Users\\Admin\\Downloads\\crawlDataTraining_selenium\\chromedriver.exe" , options=chrome_options)
 # Open URL
-driver.get("https://tiki.vn/balo-va-vali/c6000")
+# driver.get("https://tiki.vn/balo-va-vali/c6000")
 sleep(random.randint(2,4))
 links = []
-prices = []
-for i in range(2, 49):
-    # Truy cập trang Tiki có chỉ số i
-    driver.get('https://tiki.vn/balo-va-vali/c6000?page=' + str(i))
-    sleep(random.randint(1,3))
-    elems = driver.find_elements(By.CLASS_NAME , "product-item")
-    for elem in elems: 
-        link  = elem.get_attribute('href')
-        links.append(link)         
-    # elems_prices = driver.find_elements(By.CSS_SELECTOR , ".price-discount__price")
-    # for elem_price in elems_prices:
-    #     price = elem_price.text
-    #     prices.append(elem_price)
+# Khởi tạo đối tượng scraper
+scraper = TikiScraper()
+
+# Lấy danh sách liên kết
+links = scraper.get_links()
+for link in links:
+    for i in range(2, 49):
+        # Truy cập trang Tiki có chỉ số i
+            driver.get(link + '?page='+ str(i))
+            sleep(random.randint(1,3))
+            elems = driver.find_elements(By.CLASS_NAME , "product-item")
+            for elem in elems: 
+                link2  = elem.get_attribute('href')
+                links.append(link2)         
+        # elems_prices = driver.find_elements(By.CSS_SELECTOR , ".price-discount__price")
+        # for elem_price in elems_prices:
+        #     price = elem_price.text
+        #     prices.append(elem_price)
 df1 = pd.DataFrame({'link_item': links} )
-df1.to_csv('product_link_balo.csv', index=True)
+df1.to_csv('product_link_.csv', index=True)
--- crawl_category.py
+++ crawl_category.py
@@ -13,26 +13,28 @@ chrome_options.add_argument('--no-sandbox')
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
 chrome_options.add_argument(f'user-agent={user_agent}')
 
-driver = webdriver.Chrome("C:\\Users\\Admin\\Downloads\\crawlDataTraining_selenium\\chromedriver.exe" , options=chrome_options)
+# driver = webdriver.Chrome("C:\\Users\\Admin\\Downloads\\crawlDataTraining_selenium\\chromedriver.exe" , options=chrome_options)
 # Open URL
+class TikiScraper:
+    def __init__(self):
+        self.driver = webdriver.Chrome("C:\\Users\\Admin\\Downloads\\crawlDataTraining_selenium\\chromedriver.exe" , options=chrome_options)
 
-# def get_links():
+    def get_links(self):
+        # Mở trang web Tiki
+        self.driver.get("https://tiki.vn/")
+        sleep(random.randint(2,4))
 
-    # Mở trang web Tiki
-driver.get("https://tiki.vn/")
-sleep(random.randint(2,4))
+        # Tìm tất cả các đối tượng chứa link đến trang sản phẩm
+        title_elements = self.driver.find_elements(By.CLASS_NAME, 'styles__StyledItem-sc-oho8ay-0.bzmzGe')
+        link_elements = title_elements[8:]
 
-    # Tìm tất cả các đối tượng chứa link đến trang sản phẩm
-title_elements = driver.find_elements(By.CLASS_NAME, 'styles__StyledItem-sc-oho8ay-0.bzmzGe')
-link_elements = title_elements[8:]
+        # Lưu trữ các liên kết vào danh sách
+        links = [link.get_attribute("href") for link in link_elements]
+        links = links[:-1]
 
-    # Lưu trữ các liên kết vào danh sách
-links = [link.get_attribute("href") for link in link_elements]
-links = links[:-1]
-df1 = pd.DataFrame({'link_category': links} )
-df1.to_csv('link.csv', index=True)
+        # Đóng trình duyệt
+        self.driver.quit()
 
-    # Đóng trình duyệt
+        # Trả về danh sách liên kết
+        return links
 
-    # Trả về danh sách liên kết
-# return links
